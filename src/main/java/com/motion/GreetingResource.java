99431275f0d47944641f702a883139f881d3276d
Fabio Sakamoto <fabiojapa@gmail.com>
2023-12-22 15:48:09 -0300

    formatting message with liqp and posting in pubsub

--- src/main/java/com/motion/GreetingResource.java
+++ src/main/java/com/motion/GreetingResource.java
@@ -2,6 +2,7 @@ package com.motion;
 
 import static io.quarkus.arc.ComponentsProvider.LOG;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.google.api.core.ApiFuture;
 import com.google.api.core.ApiFutureCallback;
 import com.google.api.core.ApiFutures;
@@ -14,6 +15,7 @@ import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -39,15 +41,27 @@ public class GreetingResource {
 
     private Subscriber subscriber;
 
-    @GET
+    @POST
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() throws InterruptedException, IOException {
+    public String hello(JsonNode jsonNode) throws InterruptedException, IOException {
+        File file = testLiqp.getFile("liquid/test.liquid");
+        testLiqp.getFile("liquid/test.liquid");
+        testLiqp.getFile("liquid/test.liquid");
+
+        //Read file
+        String lines = Files.readString(file.toPath());
+
+        Template template = Template.parse(lines);
+        String rendered = template.render(jsonNode.toString());
+        System.out.println(rendered);
+
+
         topicName = TopicName.of(projectId, "saka-topic-test");
         Publisher publisher = Publisher.newBuilder(topicName)
             .setCredentialsProvider(credentialsProvider)
             .build();
         try {
-            ByteString data = ByteString.copyFromUtf8("my-message");// Create a new message
+            ByteString data = ByteString.copyFromUtf8(rendered); // Create a new message
             PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
             ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);// Publish the message
             ApiFutures.addCallback(messageIdFuture, new ApiFutureCallback<String>() {// Wait for message submission and log the result
@@ -64,13 +78,14 @@ public class GreetingResource {
             publisher.awaitTermination(1, TimeUnit.MINUTES);
         }
 
-        return "Hello from RESTEasy Reactive";
+        return rendered;
     }
 
-    @GET
+    @POST
     @Path("/file")
     @Produces(MediaType.TEXT_PLAIN)
-    public String helloFile() throws InterruptedException, IOException {
+    public String helloFile(JsonNode jsonNode) throws InterruptedException, IOException {
+        System.out.println("jsonNode = " + jsonNode.toString());
 
         File file = testLiqp.getFile("liquid/test.liquid");
         testLiqp.getFile("liquid/test.liquid");
@@ -80,8 +95,8 @@ public class GreetingResource {
         String lines = Files.readString(file.toPath());
 
         Template template = Template.parse(lines);
-        String rendered = template.render("name", "tobi");
+        String rendered = template.render(jsonNode.toString());
         System.out.println(rendered);
-        return "Hello from RESTEasy Reactive";
+        return rendered;
     }
 }
