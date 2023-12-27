f1b8abf4845f85befe168e377e150180be7ce3c2
Fabio Sakamoto <fabiojapa@gmail.com>
2023-12-27 13:52:32 -0300

    sqlserver debezium

--- etc/init/setup-connectors.sh
+++ etc/init/setup-connectors.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # Wait for the Connect to start
+apt update
+apt install -y curl gettext
+
 echo "Waiting for Connect to be ready"
 until curl -s http://connect:8083/ &> /dev/null
 do
   echo -n "."
   sleep 1
 done
-sleep 7
-curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://connect:8083/connectors/ -d @/connector/register-sqlserver.json
+sleep 5
 
-curl -X POST -H "Content-Type: application/json" -d @/connector/sink-connector.json http://connect:8083/connectors | jq
+envsubst < /connector/register-sqlserver.json > /connector/register-sqlserver-temp.json
+envsubst < /connector/sink-connector.json > /connector/sink-connector-temp.json
+curl -i -X POST -H "Accept:application/json" -H  "Content-Type:application/json" http://connect:8083/connectors/ -d @/connector/register-sqlserver-temp.json
+
+curl -X POST -H "Content-Type: application/json" -d @/connector/sink-connector-temp.json http://connect:8083/connectors
